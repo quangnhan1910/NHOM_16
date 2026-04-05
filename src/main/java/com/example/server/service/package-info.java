603fa20c35b5f47385c4b7561cb4bcdbd4a5376c
PhamVanPhuc - 2475 <vanphucpham2k4@gmail.com>
2026-04-05 15:25:09 +0700

    MMO-121 Quản Lý Ngân Hàng Câu Hỏi

---
+++ src/main/java/com/example/server/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package chứa các class Service chứa logic nghiệp vụ, gọi Repository và cung cấp dữ liệu cho Controller.
+ * Ví dụ: UserService, ExamService, QuestionService, ...
+ */
+package com.example.server.service;
